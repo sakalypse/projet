17776dbe8aa81cdaf488790c2d600a2ba468ea31
kevin.limacher <kevin.limacher@gmail.com>
2018-03-17 06:42:32 -0400

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20180317102944) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "students", force: :cascade do |t|
     t.string   "name"
     t.string   "lastname"
